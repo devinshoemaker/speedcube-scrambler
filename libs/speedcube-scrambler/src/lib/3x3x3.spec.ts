beac150cd1682679ebd7a52b42f4dde0a98b954a
Devin Shoemaker <devinshoe@gmail.com>
2023-04-14 11:21:30 -0500

    feat(speedcube-scrambler): add PLL scrambler (#9)
    This change adds support for random PLL scrambles with an optional PLL filter.

--- libs/speedcube-scrambler/src/lib/3x3x3.spec.ts
+++ libs/speedcube-scrambler/src/lib/3x3x3.spec.ts
@@ -1,12 +1,59 @@
 import { Scramble3x3x3 } from './3x3x3';
+import { olls } from './oll';
+import { plls } from './pll';
 
 describe('Scramble3x3x3', () => {
-  it('should generate random scramble', () => {
-    const scrambler = new Scramble3x3x3();
-    expect(scrambler.getRandomScramble().length).toBeGreaterThan(0);
+  describe('getRandomScramble', () => {
+    it('should generate random scramble', () => {
+      const scrambler = new Scramble3x3x3();
+      expect(scrambler.getRandomScramble().length).toBeGreaterThan(0);
+    });
+
+    it('should generate unique scrambles', () => {
+      const scrambler = new Scramble3x3x3();
+      const scramble1 = scrambler.getRandomScramble();
+      const scramble2 = scrambler.getRandomScramble();
+      expect(scramble1).not.toEqual(scramble2);
+    });
   });
-  it('should generate OLL scramble', () => {
-    const scrambler = new Scramble3x3x3();
-    expect(scrambler.getOllScramble().length).toBeGreaterThan(0);
+
+  describe('getOllScramble', () => {
+    it('should generate OLL scramble', () => {
+      const scrambler = new Scramble3x3x3();
+      expect(scrambler.getOllScramble().length).toBeGreaterThan(0);
+    });
+
+    it('should generate unique scrambles', () => {
+      const scrambler = new Scramble3x3x3();
+      const scramble1 = scrambler.getOllScramble();
+      const scramble2 = scrambler.getOllScramble();
+      expect(scramble1).not.toEqual(scramble2);
+    });
+
+    it('should filter OLLs', () => {
+      const ollFilter = olls.filter((oll) => oll.name === '1');
+      const scrambler = new Scramble3x3x3();
+      expect(scrambler.getOllScramble(ollFilter).length).toBeGreaterThan(0);
+    });
+  });
+
+  describe('getPllScramble', () => {
+    it('should generate PLL scramble', () => {
+      const scrambler = new Scramble3x3x3();
+      expect(scrambler.getPllScramble().length).toBeGreaterThan(0);
+    });
+
+    it('should generate unique scrambles', () => {
+      const scrambler = new Scramble3x3x3();
+      const scramble1 = scrambler.getPllScramble();
+      const scramble2 = scrambler.getPllScramble();
+      expect(scramble1).not.toEqual(scramble2);
+    });
+
+    it('should filter PLLs', () => {
+      const pllFilter = plls.filter((pll) => pll.name === '1');
+      const scrambler = new Scramble3x3x3();
+      expect(scrambler.getPllScramble(pllFilter).length).toBeGreaterThan(0);
+    });
   });
 });
---
+++ libs/speedcube-scrambler/src/lib/pll.ts
@@ -0,0 +1,91 @@
+export interface Pll {
+  id: number;
+  name: string;
+}
+
+export const plls = [
+  {
+    id: 0,
+    name: 'H',
+  },
+  {
+    id: 1,
+    name: 'Ua',
+  },
+  {
+    id: 2,
+    name: 'Ub',
+  },
+  {
+    id: 3,
+    name: 'Z',
+  },
+  {
+    id: 4,
+    name: 'Aa',
+  },
+  {
+    id: 5,
+    name: 'Ab',
+  },
+  {
+    id: 6,
+    name: 'E',
+  },
+  {
+    id: 7,
+    name: 'F',
+  },
+  {
+    id: 8,
+    name: 'Ga',
+  },
+  {
+    id: 9,
+    name: 'Gb',
+  },
+  {
+    id: 10,
+    name: 'Gc',
+  },
+  {
+    id: 11,
+    name: 'Gd',
+  },
+  {
+    id: 12,
+    name: 'Ja',
+  },
+  {
+    id: 13,
+    name: 'Jb',
+  },
+  {
+    id: 14,
+    name: 'Na',
+  },
+  {
+    id: 15,
+    name: 'Nb',
+  },
+  {
+    id: 16,
+    name: 'Ra',
+  },
+  {
+    id: 17,
+    name: 'Rb',
+  },
+  {
+    id: 18,
+    name: 'T',
+  },
+  {
+    id: 19,
+    name: 'V',
+  },
+  {
+    id: 20,
+    name: 'Y',
+  },
+];
